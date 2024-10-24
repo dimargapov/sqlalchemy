6128567a8f6d85bc37ac4370f296e15c2dd6d903
dimargapov <dimargapov@gmail.com>
2024-10-24 13:56:20 +0300

    connected to sqlalchemy, made table "users"

--- test_sqlalchemy.py
+++ test_sqlalchemy.py
@@ -1,19 +1,19 @@
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
+class Base(DeclarativeBase): pass
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    username = (String, nullable=False)
-    password = (String, nullable=False)
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
 
-def __repr__(self):
-    return f"<User(id='{self.id}', username='{self.username}')>"
+    def __repr__(self):
+        return f"<User(id='{self.id}', username='{self.username}')>"
 
-DATABASE_URL = 'postgresql+psycopg2://postgres:Dimalera0805@localhost:5432/mydatabase'
+DATABASE_URL = 'postgresql+psycopg2://postgres:Dimalera0805@localhost:5432/postgres'
 engine = create_engine(DATABASE_URL)
 
 Base.metadata.create_all(engine)
