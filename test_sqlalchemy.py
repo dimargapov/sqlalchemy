76fff9214126f5b6c3b33d302ed906bc67659fc1
dimargapov <dimargapov@gmail.com>
2024-10-24 14:39:17 +0300

    created register route, all_users route

--- test_sqlalchemy.py
+++ test_sqlalchemy.py
@@ -1,6 +1,15 @@
+from datetime import timedelta
+from flask import Flask, request, jsonify
+from collections.abc import Mapping
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'secret_key'
+jwt = JWTManager(app)
 
 class Base(DeclarativeBase): pass
 
@@ -12,8 +21,11 @@ class User(Base):
 
     def __repr__(self):
         return f"<User(id='{self.id}', username='{self.username}')>"
+    
+    def to_dict(self):
+        return {'id': self.id, 'username': self.username}
 
-DATABASE_URL = 'postgresql+psycopg2://postgres:Dimalera0805@localhost:5432/postgres'
+DATABASE_URL = 'postgresql+psycopg2://postgres:Dimalera0805@localhost:5432/db_for_users'
 engine = create_engine(DATABASE_URL)
 
 Base.metadata.create_all(engine)
@@ -21,10 +33,23 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-new_user = User(username = 'testuser', password = 'testpassword')
-session.add(new_user)
-session.commit()
+@app.route('/users')
+def show_all_users():
+    all_users = session.query(User).all()
+    user_list = [user.to_dict() for user in all_users]
+    return jsonify(user_list)
 
-all_users = session.query(User).all()
-for user in all_users:
-    print(user)
+@app.route('/register', methods=['POST'])
+def user_sign_in():
+    username = request.form['username']
+    password = request.form['password']
+    if not username or not password:
+        return jsonify({"msg": "Username or Password are required!"})
+    hashed_password = generate_password_hash(password)
+    new_user = User(username=username, password=hashed_password)
+    session.add(new_user)
+    session.commit()
+    return jsonify({"msg": "User added succesfully!"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
